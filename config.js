98ab2f03c17d601c19efd191ddf5ec06cbb87081
Moral <jeramoral@gmail.com>
2020-08-28 14:58:24 +0000

    added js

---
+++ config.js
@@ -0,0 +1,28 @@
+  function submitForm(){
+  var dress = document.getElementById("name").value
+    localStorage.setItem("name" , name)
+ var cellphone = document.getElementById("email").value
+    localStorage.setItem("email" , email)
+  }
+   function submitForm(){
+       var sneacker = document.getElementById("name").value
+    localStorage.setItem("name" , name)
+  var name = document.getElementById("Cellphone no.").value
+    localStorage.setItem("name" , name)
+ }
+   function submitForm(){
+       var T-shirt = document.getElementById("name").value
+    localStorage.setItem("name" , name)
+  var name = document.getElementById("email").value
+    localStorage.setItem("email" , email)
+  }
+   function submitForm(){
+       var tracksuit = document.getElementById("name").value
+    localStorage.setItem("name" , name)
+  var name = document.getElementById("name").value
+    localStorage.setItem("name" , name)
+ }
+   
+
+
+
